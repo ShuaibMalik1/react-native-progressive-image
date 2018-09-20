7018b84081fcae465f99f54465cfd33f1e74b3a5
shuaibmalik <malik.shuaib01@gmail.com>
2018-09-20 13:15:19 +0530

    remove duplicate declaration PropTypes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component} from 'react'
 import PropTypes from 'prop-types';
 import { Animated, View, Image, StyleSheet } from 'react-native'
 
